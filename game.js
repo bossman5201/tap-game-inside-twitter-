f6cb99da6675f631329f476910e83ffeb5110a5c
bossman5201 <139035514+bossman5201@users.noreply.github.com>
2025-03-22 00:24:48 +0530

    Update game.js

--- game.js
+++ game.js
@@ -13,34 +13,15 @@ const config = {
 let game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('tapButton', 'monad-logo.png');
+    this.load.image('tapButton', 'monad-logo.png'); // Make sure the file exists in the repo
 }
 
 function create() {
     let tapButton = this.add.image(400, 300, 'tapButton').setInteractive();
+    tapButton.setScale(0.5); // Adjust the size if needed
     tapButton.on('pointerdown', () => {
         console.log('Tapped!');
     });
 }
 
 function update() {}
-
-function buyTaps(amount) {
-    console.log(`Buying ${amount} taps...`);
-}
-
-function activateAutoTapper() {
-    console.log("Auto-Tapper Activated!");
-}
-
-function activateDoubleTap() {
-    console.log("Double Tap Activated!");
-}
-
-function checkLuckyTap() {
-    console.log("Lucky Tap Checked!");
-}
-
-function activateReferral() {
-    console.log("Referral Bonus Activated!");
-}
